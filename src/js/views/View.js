f5950f32425959acb9998db84fbdb88b567f7583
Sebastian Manco Gomez <sebasmg1393@gmail.com>
2023-05-08 14:41:11 -0500

    final consideratoins

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,16 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data the date to be render (e.g. recipe)
+   * @param {boolean} [render] if false, create markup string instead of rendering to the DOM
+   * @returns [undefined | string] A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Seb Dev
+   * @todo Finish implementation
+   */
+
   render(data, render = true) {
     // console.log(data);
     if (!data || (Array.isArray(data) && data.length == 0))
