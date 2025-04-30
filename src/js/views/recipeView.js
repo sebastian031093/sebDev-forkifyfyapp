fa9dd7e6a5691a0d196da141414c83e834a8425c
Sebastian Manco G <sebasmg1393@gmail.com>
2025-04-30 13:04:11 -0500

    sebastian manco: fraction added

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import Fraction from 'fraction.js';
 import View from './View.js';
 
 class recipeView extends View {
@@ -44,9 +44,8 @@ class recipeView extends View {
   _generateMarkup() {
     return `
         <figure class="recipe__fig">
-          <img src="${this._data.image}" alt="${
-      this._data.title
-    }" class="recipe__img" />
+          <img src="${this._data.image}" alt="${this._data.title
+      }" class="recipe__img" />
           <h1 class="recipe__title">
             <span>${this._data.title}</span>
           </h1>
@@ -57,31 +56,27 @@ class recipeView extends View {
             <svg class="recipe__info-icon">
               <use href="${icons}#icon-clock"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${
-              this._data.cookingTime
-            }</span>
+            <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookingTime
+      }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
           <div class="recipe__info">
             <svg class="recipe__info-icon">
               <use href="${icons}#icon-users"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">${
-              this._data.servings
-            }</span>
+            <span class="recipe__info-data recipe__info-data--people">${this._data.servings
+      }</span>
             <span class="recipe__info-text">servings</span>
 
             <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--update-servings" data-update-to="${
-                this._data.servings - 1
-              }">
+              <button class="btn--tiny btn--update-servings" data-update-to="${this._data.servings - 1
+      }">
                 <svg>
                   <use href="${icons}#icon-minus-circle"></use>
                 </svg>
               </button>
-              <button class="btn--tiny btn--update-servings" data-update-to="${
-                this._data.servings + 1
-              }">
+              <button class="btn--tiny btn--update-servings" data-update-to="${this._data.servings + 1
+      }">
                 <svg>
                   <use href="${icons}#icon-plus-circle"></use>
                 </svg>
@@ -96,9 +91,8 @@ class recipeView extends View {
           </div>
             <button class="btn--round btn--bookmark">
               <svg class="">
-                <use href="${icons}#icon-bookmark${
-      this._data.bookmarked ? '-fill' : ''
-    }"></use>
+                <use href="${icons}#icon-bookmark${this._data.bookmarked ? '-fill' : ''
+      }"></use>
               </svg>
             </button>
           </div>
@@ -108,8 +102,8 @@ class recipeView extends View {
           <ul class="recipe__ingredient-list">
 
             ${this._data.ingredients
-              .map(this._generateMarkupIngrediant)
-              .join('')}
+        .map(this._generateMarkupIngrediant)
+        .join('')}
           </ul>
         </div>
 
@@ -117,9 +111,8 @@ class recipeView extends View {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              this._data.publisher
-            }</span>. Please check out
+            <span class="recipe__publisher">${this._data.publisher
+      }</span>. Please check out
             directions at their website.
           </p>
           <a
@@ -142,9 +135,8 @@ class recipeView extends View {
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
-        <div class="recipe__quantity">${
-          ing.quantity ? new Fraction(ing.quantity).toString() : ''
-        }</div>
+        <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''
+      }</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
           ${ing.description}
